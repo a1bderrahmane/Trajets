e46898dd59181bd5554702bce26446fbdc405816
a1bderrahmane <124886327+a1bderrahmane@users.noreply.github.com>
2023-11-29 17:43:24 +0100

    Update Catalogue.h

--- Catalogue.h
+++ Catalogue.h
@@ -32,7 +32,7 @@ public:
     void Inserer(Trajet *ptr);
     void Afficher()const;
     void ChercherA(const char*deb,const char*fin)const;
-
+    void ChercherB(const char*deb,const char*fin);
     //----------------------------------------------------- Méthodes publiques
     // type Méthode ( liste des paramètres );
     // Mode d'emploi :
@@ -61,6 +61,12 @@ protected:
 Trajet **Catal;
 int allocated;
 int filled;
+int **matrice;
+
+private:
+void  Creer(void);
+void DFS(int* visited,int current, int destination, int path[], int pathIndex);
+
 };
 
 //-------------------------------- Autres définitions dépendantes de <Catalogue>
